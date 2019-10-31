8e3d681a527709d86759b5c6b63ae81fc9fa1c0b
cuixiuyin <1099442418@qq.com>
2019-10-31 09:37:53 +0800

    spring web flux

--- src/main/java/org/spring/webflux/WebfluxApplication.java
+++ src/main/java/org/spring/webflux/WebfluxApplication.java
@@ -1,11 +1,7 @@
 package org.spring.webflux;
 
-import org.spring.webflux.config.WebFluxConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 
 /**
  * 基于Reactor Netty实现WebFlux服务
@@ -14,12 +10,7 @@ import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 public class WebfluxApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(WebFluxConfig.class);
-        //通过ApplicationContext创建HttpHandler
-        HttpHandler httpHandler = WebHttpHandlerBuilder.applicationContext(applicationContext).build();
-        ReactorHttpHandlerAdapter httpHandlerAdapter = new ReactorHttpHandlerAdapter(httpHandler);
-        HttpServer.create("localhost", 8080).newHandler(httpHandlerAdapter).block();
-        System.in.read();
+        SpringApplication.run(WebfluxApplication.class);
     }
 
 }
